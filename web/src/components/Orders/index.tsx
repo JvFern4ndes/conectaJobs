b935aa62955e66ad5b81a0a7b93eb268c0aef2ea
João Victor <jvfernandes.contato@gmail.com>
2023-09-27 19:59:22 -0300

    feat: create Orders component

---
+++ web/src/components/Orders/index.tsx
@@ -0,0 +1,15 @@
+import { Container, Board } from "./styles";
+
+export function Orders() {
+    return (
+        <Container>
+            <Board>
+                <header>
+                    <img src="" alt="Applications Icon" />
+                    <strong>Candidaturas</strong>
+                    <span>(1)</span>
+                </header>
+            </Board>
+        </Container>
+    );
+}
