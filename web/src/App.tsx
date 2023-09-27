f963d19bc81afae213b9b65612a53089cded7f51
João Victor <jvfernandes.contato@gmail.com>
2023-09-27 17:16:01 -0300

    feat: creation of header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyles } from "./styles/GlobalStyles";
 
+import { Header } from "./components/Header";
+
 export function App() {
   return (
     <>
         <GlobalStyles />
-        <h1>Hello world</h1>
+        <Header />
     </>
   );
 }
---
+++ web/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header>
+            <div className="page-details">
+                <h1>Candidaturas</h1>
+                <h2>Acompanhe suas candidaturas</h2>
+            </div>
+        </header>
+    )
+}
