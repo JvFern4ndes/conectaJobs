556b1d704121f620236c4deae1d123732ed49f32
João Victor <jvfernandes.contato@gmail.com>
2023-10-03 17:07:57 -0300

    feat: creating the modal header

--- web/src/components/ApplicationModal/index.tsx
+++ web/src/components/ApplicationModal/index.tsx
@@ -1,4 +1,7 @@
-import { Overlay } from "./styles";
+import CloseIcon from '../../assets/images/close-icon.svg';
+import ApplicationIcon from '../../assets/images/ApplicationIcon.svg';
+
+import { Overlay, ModalBody } from "./styles";
 
 interface ApplicationModalProps {
     visible: boolean;
@@ -11,7 +14,23 @@ export function ApplicationModal({ visible }: ApplicationModalProps) {
 
     return (
         <Overlay>
-            <h1>ApplicationModal</h1>
+            <ModalBody>
+                <header>
+                    <strong>Desenvolvedor</strong>
+
+                    <button type="button">
+                        <img src={CloseIcon} alt='Ícone de fechar o modal'></img>
+                    </button>
+                </header>
+
+                <div className="status-container">
+                    <small>Status da Candidatura</small>
+                    <div>
+                        <img src={ApplicationIcon} ></img>
+                        <strong>Candidaturas</strong>
+                    </div>
+                </div>
+            </ModalBody>
         </Overlay>
     )
 }
--- web/src/components/ApplicationModal/styles.ts
+++ web/src/components/ApplicationModal/styles.ts
@@ -8,4 +8,51 @@ export const Overlay = styled.div`
     width: 100%;
     height: 100%;
     position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const ModalBody = styled.div`
+    background: #fff;
+    width: 480px;
+    border-radius: 8px;
+    padding: 32px;
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        strong {
+            font-size: 24px;
+        }
+
+        button {
+            display: flex;
+            border: 0;
+            background: transparent;
+        }
+    }
+
+    .status-container {
+        margin-top: 32px;
+
+        small {
+            font-size: 14px;
+            opacity: 0.8;
+        }
+
+        div {
+            margin-top: 8px;
+            display: flex;
+            align-items: center;
+            gap: 8px;
+        }
+
+        img {
+            width: 32px;
+        }
+    }
+
 `;
